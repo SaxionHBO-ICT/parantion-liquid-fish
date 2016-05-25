44eaf264abb9129dc2f1c66ef15160f0bd549268
Rick Harmelink <rimihaeg@live.nl>
2016-05-25 19:30:20 +0200

    created basic layout

---
+++ app/src/main/java/liquidfish/easion/view/ContactScreen.java
@@ -0,0 +1,15 @@
+package liquidfish.easion.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import liquidfish.easion.R;
+
+public class ContactScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_screen);
+    }
+}
